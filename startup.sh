3e6d0155d1eb24f3117e27f31440ca37c475336f
kylesmart2 <71296242+kylesmart2@users.noreply.github.com>
2023-06-27 16:21:44 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ printf "\n/********************\nUpdating packages:\n\n"
 sudo apt-get update && sudo apt-get upgrade -y
 
 printf "\n/********************\nInstalling Ansible:\n\n"
-sudo apt-get install ansible
+sudo apt-get install ansible -y
 
 printf "\n/********************\nAnsible version:\n\n"
 ansible --version
